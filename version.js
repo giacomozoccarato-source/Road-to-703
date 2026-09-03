f95dc004678856d0677e7342774b818fdd7b9f3d
giacomozoccarato-source <giacomo.zoccarato@gmail.com>
2026-09-03 10:58:04 +0200

    Add files via upload

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 "use strict";
-globalThis.ROAD703_CONFIG = Object.freeze({ APP_VERSION: "V57.1", PLAN_REVISION: "2026-09-03a" });
+globalThis.ROAD703_CONFIG = Object.freeze({ APP_VERSION: "V58", PLAN_REVISION: "2026-09-03b" });
