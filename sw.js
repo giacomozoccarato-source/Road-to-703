8fc3aad6b408464ec47da9b474b32d9eb443400b
giacomozoccarato-source <giacomo.zoccarato@gmail.com>
2026-09-07 12:52:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("./version.js?v=69b");
+importScripts("./version.js?v=68");
 const APP_VERSION=self.ROAD703_CONFIG.APP_VERSION;
 const CACHE = `road703-${APP_VERSION}`;
 const ASSETS = [
--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 "use strict";
-globalThis.ROAD703_CONFIG = Object.freeze({ APP_VERSION: "V69", PLAN_REVISION: "2026-09-07f" });
+globalThis.ROAD703_CONFIG = Object.freeze({ APP_VERSION: "V68", PLAN_REVISION: "2026-09-07d" });
