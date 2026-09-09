f282e142207d2c3e3041de0c4db20f8224b72e72
giacomozoccarato-source <giacomo.zoccarato@gmail.com>
2026-09-09 10:16:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-importScripts("./version.js?v=67.2");
+importScripts("./version.js?v=68");
 const APP_VERSION=self.ROAD703_CONFIG.APP_VERSION;
-const CACHE = `road703-${APP_VERSION}-activities1`;
+const CACHE = `road703-${APP_VERSION}-intervals-start1`;
 const ASSETS = [
   "./",
   "./version.js",
