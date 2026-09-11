6feddbf1a4a714aa14f125f94ca9f50c5392c55e
giacomozoccarato-source <giacomo.zoccarato@gmail.com>
2026-09-11 16:30:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-importScripts("./version.js?v=68.6.4");
+importScripts("./version.js?v=68.6.5");
 const APP_VERSION=self.ROAD703_CONFIG.APP_VERSION;
-const CACHE = `road703-${APP_VERSION}-activities-separated`;
+const CACHE = `road703-${APP_VERSION}-swim-variety`;
 const ASSETS = [
   "./",
   "./version.js",
--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 "use strict";
-globalThis.ROAD703_CONFIG = Object.freeze({ APP_VERSION: "V68.6.4", PLAN_REVISION: "2026-09-11-activities-separated" });
+globalThis.ROAD703_CONFIG = Object.freeze({ APP_VERSION: "V68.6.5", PLAN_REVISION: "2026-09-11-swim-variety" });
